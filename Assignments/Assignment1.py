a6ddb8f7442fcc6e7fbd288b661a057c1399c3e9
Aarav Shah <aaravshah820@gmail.com>
2026-01-27 18:46:40 -0600

    bug fixes
    fixed stdout printing, error messages, mult -> mul, integer division, and command parsing

--- Assignments/Assignment1.py
+++ Assignments/Assignment1.py
@@ -1,27 +1,31 @@
 import sys
 
-# A stack class with push, pop, and peek methods (from class demo)
 class Stack:
-    pointer = -1
+    """
+    A stack class with push, pop, and peek methods (from class demo)
+    """
     def __init__(self, length):
         self.length = length
         self.stack = [None] * length
+        self.pointer = -1
     def pop(self):
         if self.pointer == -1:
-            print("Stack is empty", file=sys.stderr)
+            print("Stack is empty, exiting", file=sys.stderr)
+            exit(2)
         value = self.stack[self.pointer]
         self.stack[self.pointer] = None
         self.pointer -= 1
         return value
     def push(self, value):
         if self.pointer == self.length - 1:
-            print("Stack is full", file=sys.stderr)
-            return
+            print("Stack is full, exiting", file=sys.stderr)
+            exit(2)
         self.pointer += 1
         self.stack[self.pointer] = value
     def peek(self):
         if self.pointer == -1:
-            print("Stack is empty", file=sys.stderr)
+            print("Stack is empty, exiting", file=sys.stderr)
+            exit(2)
         value = self.stack[self.pointer]
         return value
 
@@ -36,7 +40,7 @@ def sub(stack):
     num2 = stack.pop()
     stack.push(num2 - num1)
 
-def mult(stack):
+def mul(stack):
     num1 = stack.pop()
     num2 = stack.pop()
     stack.push(num2 * num1)
@@ -44,8 +48,7 @@ def mult(stack):
 def div(stack):
     num1 = stack.pop()
     num2 = stack.pop()
-    stack.push(num2 / num1)
-
+    stack.push(num2 // num1)
     
 # Get CLI argument for txt file 
 text_file = ""
@@ -59,30 +62,31 @@ else:
 try:
     with open(text_file, 'r') as file:
         command_list = file.read().split('\n')
-        print(command_list)
-
         # Create 100-length stack
         stack = Stack(100)
+        
         line_number = 0
-
         for command in command_list:
             line_number += 1
-            if 'push' in command:
+            if command.startswith('push '):
                 number = int(command.split(' ')[1])
                 stack.push(number)
-            elif 'pop' in command:
-                print(stack.pop())
-            elif 'add' in command:
+            elif command == 'pop':
+                stack.pop()
+            elif command == 'add':
                 add(stack)
-            elif 'sub' in command:
+            elif command == 'sub':
                 sub(stack)
-            elif 'mult' in command:
-                mult(stack)
-            elif 'div' in command:
+            elif command == 'mul':
+                mul(stack)
+            elif command == 'div':
                 div(stack)
             else:
                 print(f"Error at line number: {line_number}", file=sys.stderr)
 
+        # Print to stdout
+        print("Final top of stack:", stack.peek(), file=sys.stdout)
+        
 except FileNotFoundError:
     print(f"Error: A FileNotFound error occurred", file=sys.stderr)
     sys.exit(1)
@@ -90,12 +94,6 @@ except FileNotFoundError:
 except IOError as e:
     print(f"Error: An I/O error occurred: {e}", file=sys.stderr)
     sys.exit(1)
-
-
-
-
-# Implement add, sub, mult, and div methods
-
 
 
 
